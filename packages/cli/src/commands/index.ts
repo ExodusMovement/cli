37996794456f334ed604e1ff5df8bf968ddd2a18
Matias Lang <matias.la@exodus.io>
2022-03-07 11:06:07 -0300

    Change references to non-forked packages
    Use @exodus/ packages instead of @react-native-community ones when
    possible.

--- packages/cli/src/commands/index.ts
+++ packages/cli/src/commands/index.ts
@@ -11,7 +11,7 @@ import info from './info/info';
 import config from './config/config';
 import init from './init';
 import doctor from './doctor';
-import profileHermes from '@react-native-community/cli-hermes';
+import profileHermes from '@exodus/cli-hermes';
 
 export const projectCommands = [
   start,
