b3184fda52492f84eda10034f8cddb38ca95c8cc
Matias Lang <matias.la@exodus.io>
2023-07-26 16:55:22 -0300

    cli-platform-ios: remove dependency on fast-xml-parser
    make getConfigurationScheme() return "Debug" in most cases.

--- packages/cli-platform-ios/src/tools/getConfigurationScheme.ts
+++ packages/cli-platform-ios/src/tools/getConfigurationScheme.ts
@@ -1,19 +1,8 @@
-import {getBuildConfigurationFromXcScheme} from './getBuildConfigurationFromXcScheme';
-
 interface Args {
   scheme?: string;
   mode: string;
 }
 
-export function getConfigurationScheme(
-  {scheme, mode}: Args,
-  sourceDir: string,
-) {
-  if (scheme && mode) {
-    return mode;
-  } else if (scheme) {
-    return getBuildConfigurationFromXcScheme(scheme, mode, sourceDir);
-  }
-
+export function getConfigurationScheme({mode}: Args, _sourceDir: string) {
   return mode || 'Debug';
 }
